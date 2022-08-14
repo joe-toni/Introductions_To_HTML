327dc6de1ab4fe357bebe7bc5b1f7198b58ee9b3
joe-toni <joefaburrieta@gmail.com>
2022-08-14 15:09:53 -0700

    implemented the use of local storage for communication between child and parent frame

--- render.js
+++ render.js
@@ -1,18 +1,6 @@
-let workarea = document.getElementById("current");
-
 var opening = 
 `
-<!DOCTYPE html>
-<html lang="en-us">
-<head>
-    <title>HTML Playground</title>
-    <link rel="icon" type="image/x-icon" href= "favicon.ico">
-    <meta charset="UTF-8">
-    <meta name="author" content="Jose Faburrieta">
-    <meta name="description" content="A personal project meant for checking out some HTML components">
-    <meta name="keywords" content="HTML, testing, elements">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
+<html>
 <body>
 `;
 
@@ -30,26 +18,5 @@ function setSubject(name)
 {
     let subject = document.getElementById(name);
     total =  opening + subject.dataset.content + ending;
-    workarea.value = subject.dataset.content;
-
-
-}
-
-function current() 
-{
-    let content = document.getElementById("current").value;
-    total = opening + content + ending;
-}
-
-function display()
-{
-    let location = document.getElementById("location");
-    location.setAttribute("srcdoc", total);
-}
-
-function clearSample()
-{
-    workarea.value = "";
-    total="";
-    display();
+    localStorage.setItem('subject', total);
 }
